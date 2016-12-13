b655da3fd3fa6a7a1b1d4555dead71b6e4712afc
faxa94 <andre.chasi@gmail.com>
2016-12-13 16:50:19 -0500

    proyecto version 1

---
+++ ecmascript/script.js
@@ -0,0 +1,13 @@
+// Code goes here
+
+function cambiaColor(color) 
+{ 
+document.getElementById('presentacion').style.background=color;
+} 
+function cambiaColor1(color) 
+{ 
+document.getElementById('global').style.background=color;
+} 
+function myFunction(x) {
+    x.style.background = "yellow";
+}
